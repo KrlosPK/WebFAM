b84220f6d3ef2c3fe9301dc1c0f984cac6f0187c
KrlosPK <centille095@gmail.com>
2022-12-01 22:49:40 -0500

    added services json

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,24 +2,28 @@ import './Home.css';
 
 // Components
 import { Button } from '../Assets/Button';
+import { Services } from './Services';
 
 const Home = () => {
 	return (
-		<div className='container'>
-			<h1>
-				SOLDANDO SUEÑOS
-				<span>SOLDANDO SUEÑOS</span>
-			</h1>
-			<p>
-				Diseñamos, fabricamos y realizamos el montaje de estructuras
-				metálicas. Impermeabilizamos superficies con soluciones
-				asfálticas.
-			</p>
-			<div className='buttons'>
-				<Button text='Contáctanos »' />
-				<Button text='⚡ Quiénes somos?' />
+		<>
+			<div className='container'>
+				<h1>
+					SOLDANDO SUEÑOS
+					<span>SOLDANDO SUEÑOS</span>
+				</h1>
+				<p>
+					Diseñamos, fabricamos y realizamos el montaje de estructuras
+					metálicas. Impermeabilizamos superficies con soluciones
+					asfálticas.
+				</p>
+				<div className='buttons'>
+					<Button text='Contáctanos »' />
+					<Button text='⚡ Quiénes somos?' />
+				</div>
 			</div>
-		</div>
+			<Services />
+		</>
 	);
 };
 
---
+++ src/components/Home/Services.jsx
@@ -0,0 +1,21 @@
+import './Services.css';
+
+// JSON
+import data from './services.json';
+
+const Services = () => {
+	return (
+		<div className='services'>
+			{data.map(({ id, name, url }) => (
+				<div className='service' key={id}>
+					<div className='service__image'>
+						<img src={url} alt={name} />
+					</div>
+					<div className='service__text'>{name}</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export { Services };
