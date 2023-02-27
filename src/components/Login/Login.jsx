11203fa21b9fbcf25ad68893a3d184382165eba4
KrlosPK <centille095@gmail.com>
2023-02-26 22:50:25 -0500

    fixed netlify linking to github repo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import './Login.css'
+import './login.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button, Button2, Input, validateMail, validatePassword, API_URL, Navbar, ResponsiveNav } from '../Utils'
 
