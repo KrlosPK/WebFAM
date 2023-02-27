5d58669a7c5982e05a075badcb88c8b057113d02
KrlosPK <centille095@gmail.com>
2023-02-26 21:04:04 -0500

    doing accordion

--- src/components/Utils/OpenCard/OpenCard.jsx
+++ src/components/Utils/OpenCard/OpenCard.jsx
@@ -1,14 +1,27 @@
 import './OpenCard.css'
 
+//? Hooks
+import { useState } from 'react'
+
 //? Icons
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
 
 const OpenCard = ({ header, text }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
+
   return (
-    <div className='open-card'>
+    <div className='open-card' onClick={handleExpandClick}>
       <h3 className='open-card__header'>{header}</h3>
       <p className='open-card__text'>{text}</p>
-      <FaAngleDown className='open-card__icon'/>
+      {expanded ? (
+        <FaAngleUp className='open-card__icon' />
+      ) : (
+        <FaAngleDown className='open-card__icon' />
+      )}
     </div>
   )
 }
