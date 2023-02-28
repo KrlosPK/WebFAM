0fc75465a1de81ba00be56e77c2859936d99c646
KrlosPK <centille095@gmail.com>
2023-02-28 16:12:29 -0500

    mapped several components for better practices

--- src/components/Home/Team/Team.jsx
+++ src/components/Home/Team/Team.jsx
@@ -3,7 +3,7 @@ import './Team.css'
 //* Components
 import { MiniCard } from '../../Utils/MiniCard/MiniCard'
 
-const Team = () => {
+const Team = ({ src, alt, header, text, textColor, animation, borderRadius }) => {
   return (
     <section className='team' id='nuestroEquipo'>
       <h2 className='team__title'>Nuestro equipo</h2>
@@ -13,78 +13,18 @@ const Team = () => {
         metálicas para el sector industrial y doméstico.
       </p>
       <div className='colleagues'>
-        <MiniCard
-          src='/avatar1.png'
-          alt='Empleado de FADEMET'
-          header='Ramiro Urrego'
-          text='Director General'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar2.png'
-          alt='Empleado de FADEMET'
-          header='Mauricio Urrego'
-          text='Jefe de Operaciones'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar3.png'
-          alt='Empleado de FADEMET'
-          header='Dulfari Urrego'
-          text='Soldador'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar4.png'
-          alt='Empleado de FADEMET'
-          header='Hernán Velásquez'
-          text='Soldador'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar5.png'
-          alt='Empleado de FADEMET'
-          header='Pedro Arrieta'
-          text='Ayudante de Soldadura'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar6.png'
-          alt='Empleado de FADEMET'
-          header='Juan Pablo Zuleta'
-          text='Ayudante de Soldadura'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar7.png'
-          alt='Empleado de FADEMET'
-          header='Danilo Zapata'
-          text='Ayudante de Soldadura'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
-        <MiniCard
-          src='/avatar8.png'
-          alt='Empleado de FADEMET'
-          header='Karen Gomez'
-          text='Recursos Humanos'
-          textColor='var(--icon-color)'
-          animation='zoom-in-up'
-          borderRadius='50%'
-        />
+        {text.map((teammate, i) => (
+          <MiniCard
+            key={teammate}
+            src={src[i]}
+            alt={alt[i]}
+            header={header[i]}
+            text={teammate}
+            textColor={textColor[i]}
+            animation={animation[i]}
+            borderRadius={borderRadius[i]}
+          />
+        ))}
       </div>
     </section>
   )
