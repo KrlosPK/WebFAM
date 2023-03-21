6e6adc494b8e4a9c704be387c18f0b6d633b7ebd
KrlosPK <centille095@gmail.com>
2023-03-21 18:24:12 -0500

    add provide animated icons

--- src/components/Utils/MiniCard/MiniCard.jsx
+++ src/components/Utils/MiniCard/MiniCard.jsx
@@ -1,6 +1,7 @@
 import './MiniCard.css'
 
 // ? Libraries
+import { useEffect, useRef } from 'react'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
@@ -13,22 +14,66 @@ const MiniCard = ({
   text,
   animation,
   textColor = 'black',
-  borderRadius = '0'
+  borderRadius = '0',
+  isVideo = false,
+  controls = false
 }) => {
   AOS.init({ duration: 700 })
 
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            videoRef.current.play()
+          } else {
+            videoRef.current.pause()
+          }
+        })
+      },
+      { threshold: 0.5 }
+    )
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current)
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current)
+      }
+    }
+  }, [])
+
   return (
     <div className='mini-card flex' data-aos={animation}>
-      <LazyLoadImage
-        className='mini-card__image'
-        width={64}
-        height={64}
-        src={src}
-        effect='blur'
-        loading='lazy'
-        alt={alt}
-        style={{ borderRadius }}
-      />
+      {isVideo
+        ? (
+          <video
+            ref={videoRef}
+            controls={controls}
+            className='mini-card__image'
+            width={64}
+            height={64}
+            style={{ borderRadius }}
+          >
+            <source src={src} type='video/mp4' />
+          </video>
+        )
+        : (
+          <LazyLoadImage
+            className='mini-card__image'
+            width={64}
+            height={64}
+            src={src}
+            effect='blur'
+            loading='lazy'
+            alt={alt}
+            style={{ borderRadius }}
+          />
+        )}
       <div className='mini-card__header'>{header}</div>
       <div className='mini-card__text' style={{ color: textColor }}>
         {text}
