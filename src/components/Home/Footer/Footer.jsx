90c853894ac60aaadd9a78b1e7967862675a3bf3
KrlosPK <centille095@gmail.com>
2023-03-27 15:50:04 -0500

    improve footer

--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Footer = () => {
+  const date = new Date()
+  const year = date.getFullYear()
   return (
     <footer>
-      <div className='office-hours'>
+      <section className='office-hours'>
         <h2>Horario de atención</h2>
         <ul className='office-hours__lista'>
           <li className='office-hours__item'>
@@ -19,8 +21,21 @@ const Footer = () => {
             <strong>Domingo y Festivos</strong> Cerrado
           </li>
         </ul>
-      </div>
-      <div className='footer__contact-links'>
+      </section>
+      <section className='footer__contact-info'>
+        <ul className='footer__contact-info-list'>
+          <li className='footer__contact-item'>
+            <strong>Dirección:</strong> Calle 61A # 55A - 29
+          </li>
+          <li className='footer__contact-item'>
+            <strong>Teléfono:</strong> +57 314 756 1960
+          </li>
+          <li className='footer__contact-item'>
+            <strong>Correo:</strong> fademetmontajes@gmail.com
+          </li>
+        </ul>
+      </section>
+      <section className='footer__contact-links'>
         <a
           href='https://api.whatsapp.com/send/?phone=573147561960&text=¡Hola!%20Quisiera%20saber%20más%20sobre%20sus%20servicios.&type=phone_number&app_absent=0'
           target={'_blank'}
@@ -49,9 +64,9 @@ const Footer = () => {
             alt='Página de INSTAGRAM de Fademet Montajes'
           />
         </a>
-      </div>
-      <div className='footer__bottom'>
-        <div className='footer__logo'>
+      </section>
+      <section className='footer__bottom'>
+        <article className='footer__logo'>
           <Link to='/'>
             <LazyLoadImage
               src='/logotype-small.png'
@@ -61,14 +76,14 @@ const Footer = () => {
               alt='Logo de la empresa FADEMET'
             />
           </Link>
-          <p>Copyright © 2023 FADEMET, Inc.</p>
-        </div>
-        <div className='footer__copyright'>
+          <p>Copyright © {year} FADEMET S.A.S</p>
+        </article>
+        <article className='footer__copyright'>
           <Link to='/'>Aspectos legales</Link>
           <Link to='/'>Política de privacidad</Link>
           <Link to='/'>Seguridad</Link>
-        </div>
-      </div>
+        </article>
+      </section>
     </footer>
   )
 }
